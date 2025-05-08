2fd33304f748faacbd67047e4e6d363d27abcb3d
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2025-05-08 10:35:18 +0300

    readme

--- sgeg/src/main/kotlin/Main.kt
+++ sgeg/src/main/kotlin/Main.kt
@@ -25,10 +25,9 @@ val exclude: List<String> = listOf(
 fun main() {
     val file: InputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("repositories.json")
     val jsonText: String = file.reader().readText()
-    val repositories: List<Repository> = Gson().fromJson(jsonText, Array<Repository>::class.java).filter { !exclude.contains(it.name) }
-    repositories.sortedWith { repository1, repository2 ->
-        repository1.name.compareTo(repository2.name)
-    }
+    val repositories: List<Repository> = Gson().fromJson(jsonText, Array<Repository>::class.java)
+        .filter { !exclude.contains(it.name) }
+        .sortedWith { repository1, repository2 -> repository1.name.compareTo(repository2.name) }
     File("REPOSITORIES.md").writer().use { writer ->
         writer.write("""
         # ${repositories.size} references to study game engines
